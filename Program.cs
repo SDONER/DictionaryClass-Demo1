52bb9c7d01ac9e4a348ef7e167edd2c12bbee42a
SDONER <serpil_4984@hotmail.com>
2021-01-23 19:03:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryClass_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ad = new Dictionary<string, int>();
+
+            ad.Add("Ahmet",21);
+            ad.Add("Sinem",23);
+            ad.Add("Gamze",25);
+            ad.Add("Serpil",29);
+
+            foreach (var readItem in ad)
+            {
+                Console.WriteLine(readItem);
+            }
+
+            
+        }
+    }
+}
